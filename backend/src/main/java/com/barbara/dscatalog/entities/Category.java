63d6a12b109944cc3256a20fcb48f7aa50059330
BarbaraMAlmeida <barbara.nicollealmeida@gmail.com>
2023-11-02 18:15:16 -0300

    Criação banco de dados h2

--- backend/src/main/java/com/barbara/dscatalog/entities/Category.java
+++ backend/src/main/java/com/barbara/dscatalog/entities/Category.java
@@ -1,12 +1,23 @@
 package com.barbara.dscatalog.entities;
 
+
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(length = 155)
     private String name;
 
+    public Category() {
+
+    }
     public Category(Long id, String name) {
         this.id = id;
         this.name = name;
