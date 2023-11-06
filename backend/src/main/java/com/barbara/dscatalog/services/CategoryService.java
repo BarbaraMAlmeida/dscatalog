7061391e43cbc034fe644412b32125074746f4f3
BarbaraMAlmeida <barbara.nicollealmeida@gmail.com>
2023-11-06 19:23:11 -0300

    feat: Criando exception

--- backend/src/main/java/com/barbara/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/barbara/dscatalog/services/CategoryService.java
@@ -3,11 +3,14 @@ package com.barbara.dscatalog.services;
 import com.barbara.dscatalog.dto.CategoryDTO;
 import com.barbara.dscatalog.entities.Category;
 import com.barbara.dscatalog.repositories.CategoryRepository;
+import com.barbara.dscatalog.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -19,4 +22,12 @@ public class CategoryService {
         List<Category> list =  repository.findAll();
         return list.stream().map(item -> new CategoryDTO(item)).collect(Collectors.toList());
     }
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+        return new CategoryDTO(entity);
+    }
+
+
 }
